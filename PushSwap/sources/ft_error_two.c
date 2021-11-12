af6319dfae3036f568741925b0637cad4ab882d9
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2021-11-12 16:51:21 +0100

    wheel on the tryal

---
+++ PushSwap/sources/ft_error_two.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error_two.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanfeli <jsanfeli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 16:12:57 by jsanfeli          #+#    #+#             */
+/*   Updated: 2021/11/05 11:39:33 by jsanfeli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ps_header.h"
+
+static int ft_is_digit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (-1);
+}
+
+int ft_check_sign(int argc, const char **str)
+{
+	char **aux;
+	int	i;
+	int count;
+	int k;
+
+	k = 0;
+	count = 0;
+	aux = (char **)str;
+	while(count < argc)
+	{
+		i = 0;
+		while (aux[count][i])
+		{
+			k = (int)aux[count][i];
+			if(k ==  43 && (ft_is_digit((int)aux[count][i + 1]) == -1))
+				return(-1);
+			if(k == 45 && (ft_is_digit((int)aux[count][i + 1]) == -1))
+				return(-1);
+			i++;
+		}
+		count++;
+	}
+	return(1);
+}
