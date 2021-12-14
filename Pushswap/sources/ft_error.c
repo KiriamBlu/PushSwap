9434f89ec1c1a2b42c9ed41023259ad877c34bab
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2021-12-14 17:22:21 +0100

    BAD

---
+++ Pushswap/sources/ft_error.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanfeli <jsanfeli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 12:33:12 by jsanfeli          #+#    #+#             */
+/*   Updated: 2021/12/02 13:25:55 by jsanfeli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ps_header.h"
+
+int	ft_get_no_dup(t_stack *stack)
+{
+	t_stack	*aux;
+	size_t	check;
+	size_t	count;
+	size_t	tmp;
+
+	count = 0;
+	aux = NULL;
+	aux = stack;
+	while (count < stack->index_a)
+	{
+		tmp = 0;
+		aux = stack;
+		check = 0;
+		while (tmp < stack->index_a)
+		{
+			if (aux->a[tmp] == stack->a[count])
+				check++;
+			if (check > 1)
+				return (-1);
+			tmp++;
+		}
+		count++;
+	}
+	return (1);
+}
+
+int	ft_not_short(t_stack *stack)
+{
+	size_t	tmp;
+	size_t	aux;
+
+	tmp = 0;
+	aux = stack->index_a;
+	aux = aux - 1;
+	while (tmp < aux)
+	{
+		if (stack->a[tmp] > stack->a[tmp + 1])
+			return (1);
+		tmp++;
+	}
+	return (-1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	cont;
+
+	cont = 0;
+	while (s1[cont] && s2[cont])
+	{
+		if (s1[cont] != s2[cont])
+			return (1);
+		cont++;
+	}
+	return (-1);
+}
+
+int	ft_get_nothing_else_num(int argc, char const **argv)
+{
+	int		i;
+	int		k;
+	char	**aux;
+
+	i = 1;
+	aux = (char **)argv;
+	while (i < argc)
+	{
+		k = 0;
+		while (aux[i][k])
+		{
+			if (ft_isnum(aux[i][k++]) != 1)
+				return (-1);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_isnum(int c)
+{
+	if ((c >= 48 && c <= 57) || c == 45 || c == 43 || c == ' ')
+		return (1);
+	return (-1);
+}
---
+++ Pushswap/sources/push_swap.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanfeli <jsanfeli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 12:25:40 by jsanfeli          #+#    #+#             */
+/*   Updated: 2021/12/06 17:09:46 by jsanfeli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ps_header.h"
+
+void	ft_error_message(t_stack *stack)
+{
+	ft_free_list(stack);
+	write(1, "Error\n", 6);
+	exit (0);
+}
+
+void	ft_args_managment(int argc, char const **argv, t_stack *stack)
+{
+	size_t	j;
+
+	j = 0;
+	if (argc == 1)
+		exit (0);
+	ft_mount_list(argc, argv, stack);
+	ft_get_numlow(stack);
+	if (ft_check_sign(argc, argv) == -1)
+		ft_error_message(stack);
+	if (ft_get_nothing_else_num(argc, argv) == -1)
+		ft_error_message(stack);
+	if (ft_get_no_dup(stack) == -1)
+		ft_error_message(stack);
+	if (ft_not_short(stack) == -1)
+		exit(0);
+	return ;
+}
+
+void	ft_free_list(t_stack *stack)
+{
+	free(stack->a);
+	free(stack->b);
+	free(stack->aux);
+	free(stack->aux_a);
+	return ;
+}
+
+int	main(int argc, char const **argv)
+{
+	t_stack		stack;
+
+	ft_args_managment(argc, argv, &stack);
+	ft_shorting(&stack);
+	ft_free_list(&stack);
+	return (0);
+}
