3bed53a987c12c963e5021d650540dcfaa00e0d0
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2021-12-22 13:42:32 +0100

    done and 0 leaks

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jsanfeli <jsanfeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:25:40 by jsanfeli          #+#    #+#             */
-/*   Updated: 2021/12/06 17:09:46 by jsanfeli         ###   ########.fr       */
+/*   Updated: 2021/12/22 13:41:29 by jsanfeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@ void	ft_error_message(t_stack *stack)
 	exit (0);
 }
 
+void	ft_error_simple(void)
+{
+	write(1, "Error\n", 6);
+	exit (0);
+}
+
 void	ft_args_managment(int argc, char const **argv, t_stack *stack)
 {
 	size_t	j;
@@ -26,12 +32,12 @@ void	ft_args_managment(int argc, char const **argv, t_stack *stack)
 	j = 0;
 	if (argc == 1)
 		exit (0);
+	if (ft_check_sign(argc, argv) == -1)
+		ft_error_simple();
+	if (ft_get_nothing_else_num(argc, argv) == -1)
+		ft_error_simple();
 	ft_mount_list(argc, argv, stack);
 	ft_get_numlow(stack);
-	if (ft_check_sign(argc, argv) == -1)
-		ft_error_message(stack);
-	if (ft_get_nothing_else_num(argc, argv) == -1)
-		ft_error_message(stack);
 	if (ft_get_no_dup(stack) == -1)
 		ft_error_message(stack);
 	if (ft_not_short(stack) == -1)
